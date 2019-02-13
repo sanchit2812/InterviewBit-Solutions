0f4851465d711d09f2886957ebf55750324c830a
sanchit garg <sanchit2812@gmail.com>
2019-02-13 07:12:05 +0530

    Amazon

--- Trees/Identical Binary Trees.cpp
+++ Trees/Identical Binary Trees.cpp
@@ -30,3 +30,4 @@ int Solution::isSameTree(TreeNode* A, TreeNode* B) {
     else if(A && B) return (A->val == B->val) && isSameTree(A->left,B->left) && isSameTree(A->right , B->right);
     return 0;
 }
+ 
