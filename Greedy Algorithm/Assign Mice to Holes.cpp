463f4692f6971098b85c7ef4b90a4d58540f381a
sanchit garg <sanchit2812@gmail.com>
2019-03-02 18:34:18 +0530

    Amazon

---
+++ Greedy Algorithm/Assign Mice to Holes.cpp
@@ -0,0 +1,29 @@
+/*
+There are N Mice and N holes are placed in a straight line. 
+Each hole can accomodate only 1 mouse. 
+A mouse can stay at his position, move one step right from x to x + 1, or move one step left from x to x − 1. Any of these moves consumes 1 minute.
+Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.
+
+Example:
+
+positions of mice are:
+4 -4 2
+positions of holes are:
+4 0 5
+
+Assign mouse at position x=4 to hole at position x=4 : Time taken is 0 minutes 
+Assign mouse at position x=-4 to hole at position x=0 : Time taken is 4 minutes 
+Assign mouse at position x=2 to hole at position x=5 : Time taken is 3 minutes 
+After 4 minutes all of the mice are in the holes.
+
+Since, there is no combination possible where the last mouse's time is less than 4, 
+answer = 4.
+*/int Solution::mice(vector<int> &A, vector<int> &B) {
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    int cnt=INT_MIN;
+    for(int i=0;i<A.size();i++){
+        cnt = max(cnt,abs(B[i]-A[i]));
+    }
+    return cnt;
+}
